3409b6394df65d99a6c56a171808a97b15daae7c
Hard <125761273+HardProgrammerGod@users.noreply.github.com>
2025-03-09 17:07:11 +0200

    Update word_loader.py

--- src/word_loader.py
+++ src/word_loader.py
@@ -1,1 +1,18 @@
+import json
+from db_manager import DBManager
+
+def load_words(json_path="data/words.json"):
+    """Загрузка слова з JSON у бд"""
+    db = DBManager()
+    
+    with open(json_path, "r", encoding="utf-8") as file:
+        words = json.load(file)
+    
+    for word in words:
+        db.add_word(word["french"], word["ukrainian"])
+    
+    print("Слова завантажено!")
+
+if __name__ == "__main__":
+    load_words()
 
